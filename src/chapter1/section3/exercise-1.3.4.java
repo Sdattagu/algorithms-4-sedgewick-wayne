1495a1c5528c10eb3a620b1604d7adf288b33b1d
sdattagu <sdattagu@gmail.com>
2020-09-07 16:00:29 -0400

    initial commit

---
+++ src/chapter1/section3/exercise-1.3.4.java
@@ -0,0 +1,18 @@
+/***
+ Write a stack client 'Parentheses' that reads in a text stream from std input,
+ and uses a stack to determine whether its parentheses are properly balanced.
+ For example, your program should print 'true' for:
+ [ ( ) ] { } { [ ( ) ( ) ] ( ) }
+
+ and 'false' for:
+ [ ( ] )
+ ***/
+
+package chapter1.section3;
+
+
+public class StackClient {
+    private boolean evaluate(String stream) {
+
+    }
+}
